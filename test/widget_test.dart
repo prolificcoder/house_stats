ffdf65d0fc69ef454a36e3411e145960aadb3122
satya <satyaATmalugu.com>
2022-01-23 15:41:10 -0800

    adding gridview and remove template stuff"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:house_stats/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HouseStatsApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
