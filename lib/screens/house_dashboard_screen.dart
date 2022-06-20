e7e96bdfcd6a504067d0f1f638cc8ee83449c687
satya <satyaATmalugu.com>
2022-06-20 12:18:13 -0700

    add on tap change color

--- lib/screens/house_dashboard_screen.dart
+++ lib/screens/house_dashboard_screen.dart
@@ -11,6 +11,7 @@ class HouseDashboardScreen extends StatefulWidget {
 }
 
 class _HouseLeaderboardState extends State<HouseDashboardScreen> {
+  int _clickedIndex = -1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,54 +22,61 @@ class _HouseLeaderboardState extends State<HouseDashboardScreen> {
           create: (_) => HouseViewModel()..fetchAllHouses(),
           child: Consumer<HouseViewModel>(
             builder: (context, model, child) {
-              return _updateHouseScreen(context, model);
+              switch (model.asyncDataStatus.status) {
+                case Status.loading:
+                  return const CircularProgressIndicator();
+                case Status.error:
+                  return ErrorWidget(model.asyncDataStatus.data);
+                case Status.loaded:
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithMaxCrossAxisExtent(
+                              maxCrossAxisExtent: 200,
+                              childAspectRatio: 3 / 2,
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 20),
+                      itemCount: model.houses.length,
+                      itemBuilder: (_, index) {
+                        return Container(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                InkWell(
+                                  onTap: (() =>
+                                      setState(() => _clickedIndex = index)),
+                                  child: Text(
+                                    model.houses[index].houseName!,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium,
+                                  ),
+                                ),
+                                Text(
+                                  model.houses[index].score.toString(),
+                                  style:
+                                      Theme.of(context).textTheme.headlineSmall,
+                                ),
+                                Text(
+                                  'place: ' +
+                                      model.houses[index].position.toString(),
+                                  style: Theme.of(context).textTheme.bodyText1,
+                                ),
+                              ]),
+                          decoration: BoxDecoration(
+                            color: _clickedIndex == index
+                                ? Colors.red
+                                : Colors.cyan,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+              }
             },
           ),
         ));
-  }
-}
-
-Widget _updateHouseScreen(BuildContext context, HouseViewModel model) {
-  switch (model.asyncDataStatus.status) {
-    case Status.loading:
-      return const CircularProgressIndicator();
-    case Status.error:
-      return ErrorWidget(model.asyncDataStatus.data);
-    case Status.loaded:
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          itemCount: model.houses.length,
-          itemBuilder: (_, index) {
-            return Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      model.houses[index].houseName!,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    Text(
-                      model.houses[index].score.toString(),
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    Text(
-                      'place: ' + model.houses[index].position.toString(),
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ]),
-              decoration: BoxDecoration(
-                color: Colors.cyan,
-                borderRadius: BorderRadius.circular(15),
-              ),
-            );
-          },
-        ),
-      );
   }
 }
