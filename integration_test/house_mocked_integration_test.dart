5ab7d35997af6c92f99347c040fbc560e6e23a5f
satya <satyaATmalugu.com>
2022-07-04 10:13:02 -0700

    tests run on macos and chrome

--- integration_test/house_mocked_integration_test.dart
+++ integration_test/house_mocked_integration_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:house_stats/main.dart';
+import 'package:integration_test/integration_test.dart';
 
 main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('Integration testing house dashboard', (tester) async {
     await tester.pumpWidget(const HouseStatsApp());
     await tester.pumpAndSettle();
